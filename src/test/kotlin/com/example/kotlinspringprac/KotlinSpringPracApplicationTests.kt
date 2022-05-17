370e11dc4c434c0a4abf4760d4b9024ee89d1d68
Dooyeon Kang <danny.kang@kakaostyle.com>
2022-05-17 23:00:46 +0900

    feat: initiate project

---
+++ src/test/kotlin/com/example/kotlinspringprac/KotlinSpringPracApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.kotlinspringprac
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinSpringPracApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
